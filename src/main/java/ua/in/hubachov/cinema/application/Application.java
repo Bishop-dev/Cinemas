ee3eba3340f2b93fb969a55ee446817558e329ce
Bishop-dev <alex.bishop@yandex.ru>
2017-04-09 16:29:26 +0300

    Initial commit

---
+++ src/main/java/ua/in/hubachov/cinema/application/Application.java
@@ -0,0 +1,25 @@
+package ua.in.hubachov.cinema.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Sashko on 4/9/17.
+ */
+@SpringBootApplication
+@ComponentScan("ua.in.hubachov")
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
---
+++ src/main/java/ua/in/hubachov/web/controller/IndexController.java
@@ -0,0 +1,20 @@
+package ua.in.hubachov.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Sashko on 4/9/17.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public Object index() {
+        return "{'message': 'Hello!'}";
+    }
+
+}
